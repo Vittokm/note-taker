5a7305c27634a91a29ad8c3dbcdf4765e82238ef
Vitor Afonso <vittokm@gmail.com>
2021-03-13 17:05:36 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,13 @@ function add() {
     let note = document.createElement("DIV");
     let saveTit = document.createElement("H3");
     let noteTxt = document.createElement("P");
-    let viewButton = document.createElement("BUTTON");
+    var viewButton = document.createElement("BUTTON");
     saves.appendChild(note);
     note.appendChild(saveTit);  
     note.appendChild(noteTxt);
     note.appendChild(viewButton);
     viewButton.innerHTML = "View Note";
-    viewButton.setAttribute("onclick", "seeNote('this')");
+    viewButton.setAttribute("onclick", "seeNote()");
     saveTit.innerHTML = "Note " + i;
     noteTxt.innerHTML = txt.value;
     txt.value = "";
@@ -21,19 +21,16 @@ function add() {
 
 }
 
-function seeNote() {
-
+function seeNote(x) {
+        
     let saves = document.querySelector("#noteSaved");
     let darkLayer = document.querySelector("#main");
-
     let notes = document.querySelector("LABEL");
-    let btn = document.querySelectorAll("BUTTON");
-    
     
 
     darkLayer.style.cssText = "background-color: rgba(0, 0, 0, 0.6); transition: all 1s ease-in;";
     saves.querySelector("P").style.cssText = "overflow: visible; height: auto; transform: scale(2.5); transition: all 1s ease-in; text-shadow: 0px 0px 20px #FFF";
     
-    notes.querySelector("TEXTAREA").style.backgroundColor = "#555";
-    btn.style.display = "none";
+    notes.querySelector("TEXTAREA").style.backgroundColor = "#555"; 
+    
 }
